7ccba2f71e5d7948e99b8c8cfb8727c328828490
Ahmed Abdalsmia Owisse <ahmedabdelaamie1@gmail.com>
2026-07-25 01:46:09 +0300

    first commit

---
+++ src/components/Item.jsx
@@ -0,0 +1,16 @@
+export default function Item({ item, onDeleteItem, handleToggleItem }) {
+  // console.log("RE-Render");
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => handleToggleItem(item.id)}
+      ></input>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
